bb9641c00932c5268d07b018bce568b1eabb234b
Nguyễn Văn Lâm <60528316+NguyenLam05200@users.noreply.github.com>
2022-04-08 00:28:44 +0700

    create java project

---
+++ ManageClassroom/src/view/Index.java
@@ -0,0 +1,16 @@
+package view;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author holohoi
+ */
+public class Index {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
