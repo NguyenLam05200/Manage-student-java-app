d6b3e5cad5b4e0e5ce68d0ffa44ee17802d0d91f
Nguyễn Văn Lâm <60528316+NguyenLam05200@users.noreply.github.com>
2022-04-09 01:32:28 +0700

    Finish view Login

--- ManageClassroom/src/view/Index.java
+++ ManageClassroom/src/view/Index.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.concurrent.TimeUnit;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,9 +10,19 @@ package view;
  *
  * @author holohoi
  */
-public class Index {
+public class Index extends javax.swing.JFrame {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world");
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                Login frameDesignTest = new Login();
+//                frameDesignTest.setVisible(true);
+//            }
+//        });
+        Login frameDesignTest = new Login();
+        frameDesignTest.setVisible(true);
+        TimeUnit.SECONDS.sleep(1);
+
     }
 }
